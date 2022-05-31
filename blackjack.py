5bca9e92d0db7ec440bfb80b7276230bcdbde46b
Thomas <ths.contrera@gmail.com>
2022-05-31 19:01:40 -0300

    Import de bibliotecas e tela principal

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,32 @@
-# alguma coisa'
+# Importa bibliotecas
+import pygame
+import random
 
-uga uga 
+pygame.init()
 
-lebre
+# Gera a tela principal
+largura = 500
+altura = 400
+janela = pygame.display.set_mode((largura,altura))
+pygame.display.set_caption('Blackjack')
+
+# Inicia estruturas de dados
+game = True
+
+
+# Loop principal do jogo
+while game:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+
+
+    janela.fill((255,255,255))
+
+    pygame.display.update()
+
+
+pygame.quit()
+
+
