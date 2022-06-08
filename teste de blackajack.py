a2a9e39fd6b173344df594dffaf18d87d98e0df3
Thomas <ths.contrera@gmail.com>
2022-06-08 14:38:31 -0300

    teste de loop para jogar d nv

--- teste de blackajack.py
+++ teste de blackajack.py
@@ -96,9 +96,13 @@ branco = (250,250,250)
 lista_cartas = [a_ouros, a_espadas, a_paus, a_copas, dois_ouros, dois_espadas, dois_paus, dois_copas, tres_ouros, tres_espadas, tres_paus, tres_copas, quatro_ouros, quatro_espadas, quatro_paus, quatro_copas, cinco_ouros, cinco_espadas, cinco_paus, cinco_copas, seis_ouros, seis_espadas, seis_paus, seis_copas, sete_ouros, sete_espadas, sete_paus, sete_copas, oito_ouros, oito_espadas, oito_paus, oito_copas, nove_ouros, nove_espadas, nove_paus, nove_copas, dez_ouros, dez_espadas, dez_paus, dez_copas, j_ouros, j_espadas, j_paus, j_copas, q_ouros, q_espadas, q_paus, q_copas, k_ouros, k_espadas, k_paus, k_copas]
 dicionario = {dois_ouros: 2, dois_espadas:2, dois_paus:2, dois_copas:2, tres_ouros:3, tres_espadas:3, tres_paus:3, tres_copas:3, quatro_ouros:4, quatro_espadas:4, quatro_paus:4, quatro_copas:4, cinco_ouros:5, cinco_espadas:5, cinco_paus:5, cinco_copas:5, seis_ouros:6, seis_espadas:6, seis_paus:6, seis_copas:6, sete_ouros:7, sete_espadas:7, sete_paus:7, sete_copas:7, oito_ouros:8, oito_espadas:8, oito_paus:8, oito_copas:8, nove_ouros:9, nove_espadas:9, nove_paus:9, nove_copas:9, dez_ouros:10, dez_espadas:10, dez_paus:10, dez_copas:10, j_ouros:10, j_espadas:10, j_paus:10, j_copas:10, q_ouros:10, q_espadas:10, q_paus:10, q_copas:10, k_ouros:10, k_espadas:10, k_paus:10, k_copas:10}
 
+game = True
 jogo_rolando = True
-
 while jogo_rolando:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            jogo_rolando = False
 
     # Classe de carta
     class Carta(pygame.sprite.Sprite):
@@ -197,7 +201,6 @@ while jogo_rolando:
 
     delay = 0
 
-    game = True
     jogo_inicial = True
     jogada_player = True
     jogo_fim = False
@@ -212,6 +215,7 @@ while jogo_rolando:
 
             if event.type == pygame.QUIT:
                 game = False
+                jogo_rolando= False
 
             if jogo_inicial == True:  # Enquanto os primeiros sorteios ocorrem
                 
@@ -471,27 +475,8 @@ while jogo_rolando:
 
         mouse = pygame.mouse.get_pos()
 
-        if jogo_fim == True:
+        if jogo_fim == False:
 
-            # Verifica se estourou
-            if pontuacao_player1 > 21 or pontuacao_mesa1 > 21:
-                delay += 0.5
-                print('Estourou!')
-                #Animação  = True
-                #animação = fonte_animação.render(str("ESTOROU"), True, branco)
-                #animação = Animações(animação,275,125,0.6)
-
-            delay += 0.5  
-            # Verifica se ganhou/perdeu
-            if pontuacao_mesa > pontuacao_player:
-                perdeu = True
-            elif pontuacao_mesa == pontuacao_player:
-                empate = True
-            else:
-                ganhou = True
-
-
-        else:
             janela.blit(tela_de_fundo_ajustada, (0,0))
             # Desenha a pontuação do jogador para cada virada de carta
             for pontuacao in pp:
@@ -518,17 +503,34 @@ while jogo_rolando:
             if jogada_player == True:
                 janela.blit(botao_comprar, (600,250))
                 janela.blit(botao_segurar, (600,175))
-
+            
             #if Animação == True:    
                 #janela.blit(animação, (400,300))
                 #janela.blit(animação.text,animação.rect)
                 #animação.update()
+
+        if jogo_fim == True:
+            #janela.fill((0,0,0))
+            # Verifica se estourou
+            if pontuacao_player1 > 21 or pontuacao_mesa1 > 21:
+                delay += 0.5
+                print('Estourou!')
+                #Animação  = True
+                #animação = fonte_animação.render(str("ESTOROU"), True, branco)
+                #animação = Animações(animação,275,125,0.6)
+
+            delay += 0.5  
+            # Verifica se ganhou/perdeu
+            if pontuacao_mesa > pontuacao_player:
+                perdeu = True
+            elif pontuacao_mesa == pontuacao_player:
+                empate = True
+            else:
+                ganhou = True
+
+    
         
         pygame.display.update()
-
-        
-
-
 
 pygame.quit()
 
